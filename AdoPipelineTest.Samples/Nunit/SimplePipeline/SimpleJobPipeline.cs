5a5058b90b3472f0797e415e3aaece840a27a8d3
Blakharaz <1158859+blakharaz@users.noreply.github.com>
2026-01-03 09:00:41 +0100

    Add samples for pipelines with one stage and one job

---
+++ AdoPipelineTest.Samples/Nunit/SimplePipeline/SimpleJobPipeline.cs
@@ -0,0 +1,104 @@
+﻿using System.Reflection;
+using AdoPipelineTest.Model.Steps;
+
+namespace AdoPipelineTest.Samples.Nunit.SimplePipeline;
+
+using Is = AdoPipelineTest.Nunit.Is;
+
+public class SimpleJobPipelineTests
+{
+    [Test]
+    public void VerifyBasics()
+    {
+        var result = new PipelineTester().WithPipeline("Nunit/SimplePipeline/simple_job_pipeline.yaml").Run();
+        
+        Assert.That(result, Is.Not.Null);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result.Triggers, Is.BranchIncluded("main"));
+            Assert.That(result.AgentPool, Is.VmImage("ubuntu-latest"));
+            Assert.That(result.Stages, Has.Count.EqualTo(1));
+            Assert.That(result.Stages[0].Jobs, Has.Count.EqualTo(1));
+
+            var steps = result.Stages[0].Jobs[0].Steps;
+            Assert.That(steps, Has.Count.EqualTo(4));
+        }
+    }
+
+    [Test]
+    public void VerifyStep1()
+    {
+        var result = new PipelineTester().WithPipeline("Nunit/SimplePipeline/simple_job_pipeline.yaml").Run();
+        
+        Assert.That(result, Is.Not.Null);
+
+        var steps = result.Stages[0].Jobs[0].Steps;
+        var step1 = steps[0] as TaskStep;
+
+        Assert.That(step1, Is.Not.Null);
+        
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(step1.DisplayName, Is.EqualTo("Use .NET 8.0"));
+            Assert.That(step1.ContinueOnError, Is.False);
+            Assert.That(step1.TaskName, Is.EqualTo("UseDotNet@2"));
+        }
+    }
+
+    [Test]
+    public void VerifyStep2()
+    {
+        var result = new PipelineTester().WithPipeline("Nunit/SimplePipeline/simple_job_pipeline.yaml").Run();
+        
+        Assert.That(result, Is.Not.Null);
+        var steps = result.Stages[0].Jobs[0].Steps;
+        var step2 = steps[1] as TaskStep;
+
+        Assert.That(step2, Is.Not.Null);
+        
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(step2.DisplayName, Is.EqualTo("Restore dependencies"));
+            Assert.That(step2.ContinueOnError, Is.False);
+            Assert.That(step2.TaskName, Is.EqualTo("DotNetCoreCLI@2"));
+        }
+    }
+
+    [Test]
+    public void VerifyStep3()
+    {
+        var result = new PipelineTester().WithPipeline("Nunit/SimplePipeline/simple_job_pipeline.yaml").Run();
+        
+        Assert.That(result, Is.Not.Null);
+        var steps = result.Stages[0].Jobs[0].Steps;
+        var step3 = steps[2] as TaskStep;
+
+        Assert.That(step3, Is.Not.Null);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(step3.DisplayName, Is.EqualTo("Build"));
+            Assert.That(step3.ContinueOnError, Is.False);
+            Assert.That(step3.TaskName, Is.EqualTo("DotNetCoreCLI@2"));
+        }
+    }
+
+    [Test]
+    public void VerifyStep4()
+    {
+        var result = new PipelineTester().WithPipeline("Nunit/SimplePipeline/simple_job_pipeline.yaml").Run();
+        
+        Assert.That(result, Is.Not.Null);
+        var steps = result.Stages[0].Jobs[0].Steps;
+        var step4 = steps[3] as TaskStep;
+
+        Assert.That(step4, Is.Not.Null);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(step4.DisplayName, Is.EqualTo("Test"));
+            Assert.That(step4.ContinueOnError, Is.False);
+            Assert.That(step4.TaskName, Is.EqualTo("DotNetCoreCLI@2"));
+        }
+    }
+}
